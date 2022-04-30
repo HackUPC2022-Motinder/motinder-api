3aa8a85e4e2fc5793e4360be481d5218e1c9b7c3
Bernat Puig Font <bernat.puig@forgeflow.com>
2022-04-30 22:51:12 +0200

    Compute price and year categories

--- src/index.js
+++ src/index.js
@@ -57,7 +57,91 @@ app.get('/addELO', (req, res) => {
         console.log(elo);
     });
 });
- 
+
+// Compute results for price categories
+app.get('/compute-prices', (req, res) => {    
+    // Llega por parametro identificador moto actual
+    var ids = req.query.ids;
+    var answers = req.query.answers;
+
+    var prices = {}
+
+    const i = 0;
+    for (const id in ids) {
+        csv().fromFile(csvFilePath).then((jsonObj)=>{
+            var filtered = jsonObj.filter(a => a.id == id);
+            if (filtered.length < 1) {
+                console.log("ERROR: This motorcycle does not exist!");
+                throw exception;
+            }
+            if (filtered.length > 1) {
+                console.log("ERROR: There's more than one bike with this id!");
+                throw exception;
+            }
+            if (filtered.price_category < 0 || filtered.price_category > 4) {
+                console.log("ERROR: Category does not exist!");
+                throw exception;
+            }
+            if (isNaN( prices[filtered.price_category])) {
+                if (answers[i] == 0) prices[filtered.price_category] = -1;
+                if (answers[i] == 1) prices[filtered.price_category] = 1;
+                if (answers[i] == 2) prices[filtered.price_category] = 2;
+            }
+            else { 
+                if (answers[i] == 0) prices[filtered.price_category] = prices[filtered.price_category] - 1;
+                if (answers[i] == 1) prices[filtered.price_category] = prices[filtered.price_category] + 1;
+                if (answers[i] == 2) prices[filtered.price_category] = prices[filtered.price_category] + 2;
+            }
+        });
+
+        ++i;
+    }
+
+    res.json(prices);
+});
+
+// Compute results for year categories
+app.get('/compute-years', (req, res) => {    
+    // Llega por parametro identificador moto actual
+    var ids = req.query.ids;
+    var answers = req.query.answers;
+
+    var years = {}
+
+    const i = 0;
+    for (const id in ids) {
+        csv().fromFile(csvFilePath).then((jsonObj)=>{
+            var filtered = jsonObj.filter(a => a.id == id);
+            if (filtered.length < 1) {
+                console.log("ERROR: This motorcycle does not exist!");
+                throw exception;
+            }
+            if (filtered.length > 1) {
+                console.log("ERROR: There's more than one bike with this id!");
+                throw exception;
+            }
+            if (filtered.year_category < 0 || filtered.year_category > 4) {
+                console.log("ERROR: Category does not exist!");
+                throw exception;
+            }
+            if (isNaN( years[filtered.year_category])) {
+                if (answers[i] == 0) years[filtered.year_category] = -1;
+                if (answers[i] == 1) years[filtered.year_category] = 1;
+                if (answers[i] == 2) years[filtered.year_category] = 2;
+            }
+            else { 
+                if (answers[i] == 0) years[filtered.year_category] = years[filtered.year_category] - 1;
+                if (answers[i] == 1) years[filtered.year_category] = years[filtered.year_category] + 1;
+                if (answers[i] == 2) years[filtered.year_category] = years[filtered.year_category] + 2;
+            }
+        });
+
+        ++i;
+    }
+
+    res.json(prices);
+});
+
 // Iniciando el servidor
 app.listen(app.get('port'),()=>{
     console.log(`Server listening on port ${app.get('port')}`);
